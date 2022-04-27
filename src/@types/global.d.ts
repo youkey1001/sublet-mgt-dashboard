9920b126c379548c898f86d5153448d8ff98797c
youkey1001 <youkey_1001@yahoo.co.jp>
2022-04-27 13:08:17 +0900

    implements global types.

--- src/@types/global.d.ts
+++ src/@types/global.d.ts
@@ -3,15 +3,24 @@ type Card = {
   text: string;
 };
 
+type CardID = string;
+
 type List = {
   id: string;
   title: string;
   cards: Card[];
 };
 
+type ListID = string;
+
 type Board = {
   boardId: string;
   lists: List[];
+};
+
+type Ids = {
+  listID: ListID;
+  cardID: CardID;
 };
 
 type DragHappened = {
@@ -23,9 +32,37 @@ type DragHappened = {
   type: string;
 };
 
+// draggableId: 0
+// droppableIdEnd: 0
+// droppableIdStart: 1
+// droppableIndexEnd: 0
+// droppableIndexStart: 625a3c02ebe60beec8c8ca22
+// type: DEFAULT
+
 // draggableId: "625a3c02ebe60beec8c8ca22"
 // droppableIdEnd: "0"
 // droppableIdStart: "625a3885ebe60beec8c8ca21"
 // droppableIndexEnd: 0
 // droppableIndexStart: 0
 // type: "DEFAULT"
+
+// draggableId: "all-lists"
+// droppableIdEnd: "all-lists"
+// droppableIdStart: "1"
+// droppableIndexEnd: 0
+// droppableIndexStart: 0
+// type: "list"
+
+type ListProps = {
+  title: string;
+  listID: string;
+  cards: Card[];
+  index: number;
+};
+
+type CardProps = {
+  text: string;
+  id: string;
+  index: number;
+  listID: string;
+};
